b140fa5b6c51dfc449f8fc8fc74991e06d71bdb4
Tauseef Khan <tauseef25@gmail.com>
2025-06-26 09:04:26 -0700

    scaffolding initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <span>Hello world!</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
